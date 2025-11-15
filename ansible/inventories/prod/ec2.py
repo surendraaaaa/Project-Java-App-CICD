1ff772bb8114abd4b07171386442ad2eb46ec999
surendraaaaa <114964508+surendraaaaa@users.noreply.github.com>
2025-11-15 17:56:18 -0500

    Update and rename ec2.yml to ec2.py

---
+++ ansible/inventories/prod/ec2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import boto3
+import json
+import os
+
+def generate_inventory(instances):
+    inventory = {"_meta": {"hostvars": {}}}
+    inventory["ec2_hosts"] = {"hosts": [], "vars": {}}
+
+    for reservation in instances:
+        for instance in reservation["Instances"]:
+            if instance.get("State", {}).get("Name") != "running":
+                continue
+            ip = instance.get("PublicIpAddress")
+            if not ip:
+                continue
+
+            inventory["ec2_hosts"]["hosts"].append(ip)
+            inventory["_meta"]["hostvars"][ip] = {
+                "ansible_connection": "ssh",
+                "ansible_user": "ubuntu",
+                "ansible_ssh_private_key_file": "/home/ubuntu/.ssh/legacy-java-app-key"
+            }
+
+    return inventory
+
+def main():
+    region = os.environ.get("AWS_REGION", "us-east-2")
+    ec2 = boto3.client("ec2", region_name=region)
+    response = ec2.describe_instances(
+        Filters=[{"Name": "tag:App", "Values": ["legacy-java-app"]}]
+    )
+    inventory = generate_inventory(response["Reservations"])
+    print(json.dumps(inventory, indent=4))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# plugin: amazon.aws.ec2
+# regions:
+#   - us-east-2
+#   - us-east-1
+# filters:
+#   "tag:App": "legacy-java-app"
+# hostnames:
+#   - public-ip-address
+# compose:
+#   ansible_user: ubuntu
+#   ansible_ssh_private_key_file: /home/ubuntu/.ssh/legacy-java-app-key
+
+
